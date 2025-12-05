fb5deb06349869bc498e5c1e304f34a21e19f8d1
hurodrigues3-jpg <hurodrigues3@gmail.com>
2025-12-05 20:40:40 -0300

    Update user.py

--- api/models/user.py
+++ api/models/user.py
@@ -1,12 +1,5 @@
-class Usuario:
+class User:
     def __init__(self, id, nome, email):
         self.id = id
         self.nome = nome
         self.email = email
-
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "nome": self.nome,
-            "email": self.email
-        }
